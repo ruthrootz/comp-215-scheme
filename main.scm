eb5c694b22c715ccd2d738edfd8022b79c82fb94
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-11-30 00:58:28 +0000

    ummmm, it works

--- main.scm
+++ main.scm
@@ -1,13 +1,6 @@
 (define (append1 list1 list2)
   (if (null? list1) list2
       (if (null? list2) list1
-          (cons list1 (car list2)))))
+          (cons (car list1) (append1 (cdr list1) list2)))))
 
-(define (double x)
-        (* 2 x))
-
-
-
-(double 4)
-(append1 '(1 2 3) '(a b c))
-;; (append1 ’(1 2 3 4) ’(a b c d e f))
+(append1 '(1 2 3 4) '(a b c d e))
