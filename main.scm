623e5da47035bcaeaba234b3e3d2a08e0fd8f3d7
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-11-30 00:38:18 +0000

    a

--- main.scm
+++ main.scm
@@ -1,7 +1,7 @@
 (define (append1 list1 list2)
   (if (null? list1) list2
       (if (null? list2) list1
-          (else (cons list1 (append1 (car list2) (cdr list2))))))
+          (cons list1 (append1 (car list2) (cdr list2)))))
 
-(display '(a b c))
-(display (append1 ’(1 2 3 4) ’(a b c d e f)))
+;; (display (append1 ’(1 2 3 4) ’(a b c d e f)))
+(display 'a)
