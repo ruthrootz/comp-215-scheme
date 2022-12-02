fa7bf7e7fe119ec58d81835cc44b6177d4dcf98e
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-12-02 20:09:49 +0000

    working?!

--- main.scm
+++ main.scm
@@ -32,13 +32,12 @@
     (cond
         [(null? list1) '()]
         [(null? list2) '()]
-        [(or (null? (cdr list1)) (null? (cdr list2))) (list (car list1) (car list2))]
         [else
-             (zip (cdr list1) (cdr list2))]))
+             (cons (list (car list1) (car list2)) (zip (cdr list1) (cdr list2)))]))
 
 ; TESTS
 ; (append1 '(1 2 3 4) '(a b c d e f g))
 ; (member? 'a '(a 1 2 3 b))
 ; (member1 'a '(1 2 3 a b c))
 ; (flatten '(a b c (1 2 3) bat ((4 5 6) (d e f))))
-(zip '(1 2 3) '(a b c))
+; (zip '(1 2 3) '(a b c))
