04accc15a75c8720819638c73315a02991cc08d7
Ruth Oldja <48135255+ruthrootz@users.noreply.github.com>
2022-12-02 14:11:11 +0000

    cleanup

--- main.scm
+++ main.scm
@@ -28,7 +28,9 @@
     (cond
         [(null? list1) '()]
         [(null? list2) '()]
-        [else (zip (cdr list1) (cdr list2)) (cons (car list1) (car list2))]))
+        [else
+             (zip (cdr list1) (cdr list2))
+             (cons (car list1) (car list2))]))
 
 ;; TESTS
 ;; (append1 '(1 2 3 4) '(a b c d e f g))
